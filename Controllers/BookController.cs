587c9b1c4684432538a1ef3c93d982c6041513b7
Alyaa <66500059+alyaamansor20@users.noreply.github.com>
2025-01-17 14:29:43 +0200

    Update BookController.cs

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -44,7 +44,6 @@ namespace WordScapeAPI.Controllers
         }
 
         // PUT: api/Book/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutBook(int id, Book book)
         {
@@ -75,7 +74,6 @@ namespace WordScapeAPI.Controllers
         }
 
         // POST: api/Book
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<Book>> PostBook(Book book)
         {
